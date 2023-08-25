34b6553b7e64910d885ab5e6a4eaa85e38f043e5
spjyotiranjan <jyotiranjan.sahoo@kalvium.community>
2023-08-25 19:31:42 +0530

    Groove Done

---
+++ app.js
@@ -0,0 +1,7 @@
+var buybtn = document.getElementById("buy-now")
+var text = document.getElementById("thanks-text")
+
+buybtn.onclick=()=>{
+    text.style.display= "block"
+    buybtn.style.display= "none"
+}
